60aaaa37ae6b54fce240f70eb6374a8e4a9e8e76
Erin Koen <erin.koen@gmail.com>
2021-03-01 10:52:06 -0500

    feat: update getProvider method

--- src/utils/getProvider.ts
+++ src/utils/getProvider.ts
@@ -2,6 +2,6 @@ import { providers } from 'ethers';
 import { loadEnv } from './loadEnv';
 
 export function getProvider(network: 'MAINNET' | 'KOVAN') {
-  const key = loadEnv(`${network}_API_KEY`);
+  const key = loadEnv(`${network}_PROVIDER_API_KEY`);
   return providers.getDefaultProvider(network.toLowerCase(), { alchemy: key });
 }
