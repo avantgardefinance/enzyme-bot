daa227e4e4e3a361dff5546ccf18df35b5602fcd
Erin Koen <erin.koen@gmail.com>
2021-03-04 11:07:40 -0500

    chore: update getProvider

--- src/utils/getProvider.ts
+++ src/utils/getProvider.ts
@@ -3,5 +3,5 @@ import { loadEnv } from './loadEnv';
 
 export function getProvider(network: 'MAINNET' | 'KOVAN') {
   const node = loadEnv(`${network}_NODE_ENDPOINT`)
-  return new providers.StaticJsonRpcProvider(node, network.toLowerCase());
+  return new providers.JsonRpcProvider(node, network.toLowerCase());
 }
