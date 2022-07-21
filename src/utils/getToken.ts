7d9967757b418b44f952e501767bec685fb96b49
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-07-21 16:59:21 +0200

    fix: remove old getTokens fn

--- src/utils/getToken.ts
+++ src/utils/getToken.ts
@@ -1,11 +1,6 @@
 import { gql } from './subgraph/sdk';
 
-// pass these your subgraph endpoint
-export async function getTokens(endpoint: string) {
-  const result = await gql(endpoint).assets();
-  return result;
-}
-
+// pass your subgraph endpoint
 export async function getToken(endpoint: string, assetProperty: 'symbol' | 'id', assetPropertyValue: string) {
   const result = await gql(endpoint).assets();
   return result.assets.find((asset) => asset[assetProperty] === assetPropertyValue);
