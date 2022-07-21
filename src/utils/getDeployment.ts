98801de6714861a0860389c402e01efda13c28d1
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-07-21 16:58:49 +0200

    fix: remove old deployment fn

--- src/utils/getDeployment.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from './subgraph/sdk';
-
-// pass these your subgraph endpoint
-export async function getDeployment(endpoint: string) {
-  const result = await gql(endpoint).currentReleaseContracts();
-  return result;
-}
