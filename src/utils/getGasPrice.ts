7e0305b2ddaabfbd44e5492ea57fae2ab80388d5
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-07-22 14:12:00 +0200

    fix: polygon gas price response

--- src/utils/getGasPrice.ts
+++ src/utils/getGasPrice.ts
@@ -1,20 +1,24 @@
 import axios from 'axios';
+import { BigNumber } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
 
 export async function getGasPrice(maxWait: number) {
   try {
     const response = await axios.get('https://ethgasstation.info/json/ethgasAPI.json');
     const data = response.data.gasPriceRange;
     const price = Object.keys(data).find((price) => parseFloat(data[price]) <= maxWait);
+
     return (price || response.data.fast) / 10;
   } catch (error) {
     throw new Error(`Failed to fetch gas price data: ${error}`);
   }
 }
 
-export async function getPolygonGasPrice() {
+export async function getPolygonGasPrice(): Promise<BigNumber> {
   try {
     const response = await axios.get('https://gasstation-mainnet.matic.network/v2');
-    const price = Math.ceil(response.data.fast.maxFee);
+    const price = parseUnits(Math.ceil(response.data.fast.maxFee).toString(), 'gwei');
+
     return price;
   } catch (error) {
     throw new Error(`Failed to fetch gas price data: ${error}`);
